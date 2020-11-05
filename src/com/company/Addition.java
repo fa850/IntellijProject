52fb39c4c024a8f73219611cc6da455695651620
FA850 <fa850@nyu.edu>
2020-11-05 10:26:51 -0600

    Adding Additions

--- src/com/company/Addition.java
+++ src/com/company/Addition.java
@@ -3,6 +3,6 @@ package com.company;
 public class Addition {
     public static void main(String[] args) {
         System.out.println("Adding1");
-        System.out.println("Adding23");
+        System.out.println("Adding24");
     }
 }
