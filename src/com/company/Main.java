eb91c750df557e90ec6b7704e086f88d07d03ee3
fa850 <fa850@nyu.edu>
2020-11-05 11:03:15 -0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello WOrld");
-    System.out.println("Hello WOrld2");
+    System.out.println("Hello WOrld from master");
+    
     }
 }
